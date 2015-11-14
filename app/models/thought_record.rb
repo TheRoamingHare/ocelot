cea6f1c6a4112c6cbf1c9cf20a8f80d73964cc70
brianjp07 <parks.271@osu.edu>
2015-11-14 15:50:54 -0500

    typo

--- app/models/thought_record.rb
+++ app/models/thought_record.rb
@@ -2,7 +2,7 @@ class ThoughtRecord < ActiveRecord::Base
   # Validations
   validates :CurrentSituation, presence: true
   validates :Emotions, presence: true
-  validates :FirstEmotionStrength, presence: true,
+  validates :FirstEmotionStrength, presence: true
   validates :AutoThoughts, presence: true
   validates :AltResponse, presence: true
   validates :SecondEmotionStrength, presence: true
