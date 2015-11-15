8cbd28b6afa8ac94f444f8b89d870dc7b3346d1e
William Osler <william@oslers.us>
2015-11-15 10:31:09 -0500

    make log window pretty

--- app/helpers/activity_logs_helper.rb
+++ app/helpers/activity_logs_helper.rb
@@ -1,2 +1,10 @@
 module ActivityLogsHelper
+  def windowify(myint)
+    nice_num = myint % 13
+    return "#{nice_num}:00 #{ampm(myint)} - #{nice_num+1}:00 #{ampm(myint)}"
+  end
+
+  def ampm(myint)
+    return myint > 12 ? "PM" : "AM"
+  end
 end
