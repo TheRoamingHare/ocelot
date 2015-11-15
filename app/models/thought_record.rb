4a295276f6147189c9bf422e197ff2ed22c7c065
William Osler <william@oslers.us>
2015-11-15 01:54:17 -0500

    fix bad validations

--- app/models/thought_record.rb
+++ app/models/thought_record.rb
@@ -1,11 +1,10 @@
 class ThoughtRecord < ActiveRecord::Base
   # Validations
   validates :CurrentSituation, presence: true
+  validates :Outcome, presence: true
   validates :Emotions, presence: true
-  validates :FirstEmotionStrength, presence: true
   validates :AutoThoughts, presence: true
   validates :AltResponse, presence: true
-  validates :SecondEmotionStrength, presence: true
   validates :ActivityLog, presence: true
 
   # Associations
